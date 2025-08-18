9fee12a147de481406bdcbec7c63ba6ca4544426
Tandem <ai-agent@example.com>
2025-08-18 16:48:56 +0000

    feat: Add peregrine falcons page

---
+++ pages/peregrine-falcons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PeregrineFalcons = () => {
+  return (
+    <div>
+      <h1>Peregrine Falcons</h1>
+      <p>More information about peregrine falcons will go here.</p>
+    </div>
+  );
+};
+
+export default PeregrineFalcons;
